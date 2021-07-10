ddfd0fadb0626e20dde77fc2bdbe7297bf94284c
shaheerhussain05 <shaheerhussain444@gmail.com>
2021-07-10 10:56:04 -0700

    javascript completed
    create another javascript file
    ...use short method for non empty input fields requirements
    .... input field requirements
    ...email validation
    ...password match

--- project 1/script.js
+++ project 1/script.js
@@ -6,7 +6,7 @@ const password2 = document.getElementById('password2');
 
 //All functions
 // Function to show error
-function showEror(input,message) {
+function showError(input,message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
@@ -20,39 +20,66 @@ function showSuccess(input) {
 }
 
 //Function to check email is right
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if ( re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input,'Please provide a valid email');
+    }
 }
+
+
+// Function to check if required fields have data 
+
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        if ( input.value === '' ) {
+            console.log(input.id);
+            showError(input,`${getFieldId(input)} is required`)
+        } else {
+            showSuccess(input);
+        }
+    
+    });
+}
+
+// Function to get the id of the input field with proper case
+function getFieldId(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+// Function to check length of input field
+function checkLength(input, min, max) {
+    if ( input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if ( input.value.lenght > max ) {
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+
+    } else {
+        showSuccess(input);
+    }
+}
+
+
+// Function to check if password and confirm password
+function checkPasswordsMatch(input1, input2) {
+    if ( input1.value !== input2.value ) {
+        showError(input2,'Passwords dont match');
+    }
+}
+
+
 
 // this is event listener for the submit button
 form.addEventListener('submit',function(e) {
     e.preventDefault();
     
-    if ( username.value === '' ) {
-        showEror(username,'Username is required')
-    } else {
-        showSuccess(username);
-    }
-
-    if ( email.value === '' ) {
-        showEror(email,'Email is required')
-    } else if (!isValidEmail(email.value)) {
-        showEror(email,'Email is invalid')
-    } else  {
-        showSuccess(email);
-    }
-
-    if ( password.value === '' ) {
-        showEror(password,'Password is required')
-    } else {
-        showSuccess(password);
-    }
-
-    if ( password2.value === '' ) {
-        showEror(password2,'Confirm Password is required')
-    } else {
-        showSuccess(password2);
-    }
+    checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,10);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
 
 })
